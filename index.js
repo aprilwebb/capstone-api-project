77ad3aeb39ebde6602334214b8ade917292b41da
April Webb <derekwebb@Macintoshs-MBP-57.home>
2023-08-23 11:34:24 -0400

    Add GET and POST methods and user input field in .ejs file

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
-const API_URL = '';
+const API_URL = 'https://api.agify.io?name=';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+  res.render('index.ejs', { age: null });
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const name = req.body['nameInput'];
+    const result = await axios.get(API_URL + name);
+    res.render('index.ejs', { age: result.data.age });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
